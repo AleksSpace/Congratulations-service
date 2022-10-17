9173d3aae2f562854f160425b29daf8d11fdb95a
Алексей <Zaikin.lescha@yandex.ru>
2022-10-17 19:54:10 +0300

    Deploy

--- congratulations/congratulations/settings.py
+++ congratulations/congratulations/settings.py
@@ -65,10 +65,14 @@ WSGI_APPLICATION = 'congratulations.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': 'db.qcaxzjcffwpszpibiwyq.supabase.co',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'CpVwjrtQIesCZ27K',
+        'PORT': '5432',
+    }
 }
 
 
